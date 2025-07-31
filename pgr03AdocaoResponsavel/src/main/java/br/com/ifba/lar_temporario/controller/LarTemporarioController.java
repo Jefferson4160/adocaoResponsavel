59dd39c462dbd3129b5eb4c7eddd680f65422898
Luan Alves <alvesluanpc@gmail.com>
2025-07-31 18:52:12 -0300

    Adição do @Override e Autowired

--- pgr03AdocaoResponsavel/src/main/java/br/com/ifba/lar_temporario/controller/LarTemporarioController.java
+++ pgr03AdocaoResponsavel/src/main/java/br/com/ifba/lar_temporario/controller/LarTemporarioController.java
@@ -7,9 +7,9 @@ package br.com.ifba.lar_temporario.controller;
 import br.com.ifba.lar_temporario.entity.LarTemporario;
 import br.com.ifba.lar_temporario.service.LarTemporarioIService;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -17,32 +17,38 @@ import org.springframework.stereotype.Controller;
  * Controller responsável por lidar com requisições relacionadas ao Lar Temporário.
  */
 @Controller
-@RequiredArgsConstructor
 public class LarTemporarioController implements LarTemporarioIController {
 
-    private final LarTemporarioIService larTemporarioIService;
+    @Autowired
+    private LarTemporarioIService larTemporarioIService;
+    
     private static final Logger log = LoggerFactory.getLogger(LarTemporarioController.class);
 
+    @Override
     public LarTemporario save(LarTemporario lar) {
         log.info("Cadastrando novo Lar Temporário");
         return larTemporarioIService.save(lar);
     }
 
+    @Override
     public LarTemporario update(LarTemporario lar) {
         log.info("Atualizando Lar Temporário");
         return larTemporarioIService.update(lar);
     }
 
+    @Override
     public void delete(LarTemporario lar) {
         log.info("Deletando Lar Temporário");
         larTemporarioIService.delete(lar);
     }
 
+    @Override
     public List<LarTemporario> findAll() {
         log.info("Buscando todos os Lares Temporários");
         return larTemporarioIService.findAll();
     }
 
+    @Override
     public LarTemporario findById(Long id) {
         log.info("Buscando Lar Temporário por ID");
         return larTemporarioIService.findById(id);
