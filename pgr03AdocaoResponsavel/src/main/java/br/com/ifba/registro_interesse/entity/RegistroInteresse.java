cefd6a6d4bf68b8ac2387968719a0b8e599d076d
inquerafael <playerong@gmail.com>
2025-08-01 09:17:57 -0300

    extrutura inicical - entity registro de interesse + enum status

---
+++ pgr03AdocaoResponsavel/src/main/java/br/com/ifba/registro_interesse/entity/RegistroInteresse.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package br.com.ifba.registro_interesse.entity;
+
+import br.com.ifba.infrastructure.entity.PersistenceEntity;
+import br.com.ifba.usuario.entity.Pessoa;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+/**
+ *
+ * @author inque
+ */
+@Entity
+@Table(name = "tb_reg._interesse")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistroInteresse extends PersistenceEntity{
+    
+    @OneToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "pessoa_id", referencedColumnName = "id")
+    Pessoa pessoa;
+    
+    @ManyToOne(fetch = FetchType.LAZY) // Relacionamento (n,1): N interesse para um Animal
+    @JoinColumn(name = "animal_id", nullable = false)
+    private Animal animal;
+
+    @Enumerated(EnumType.STRING) // Armazena o status como String no bd
+    @Column(name = "status_interesse", nullable = false)
+    private StatusInteresse statusInteresse;
+    
+    @Column(name = "questionário", columnDefinition = "TEXT")
+    private String questionário;
+}
---
+++ pgr03AdocaoResponsavel/src/main/java/br/com/ifba/registro_interesse/entity/StatusInteresse.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package br.com.ifba.registro_interesse.entity;
+
+/**
+ *
+ * @author inque
+ */
+public enum StatusInteresse {
+    EM_ANALISE,
+    APROVADO,
+    REJEITADO,
+    CONCLUIDO
+}
